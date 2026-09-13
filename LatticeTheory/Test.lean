9c64d0f2a0cac500a19eaeb5811e84b67e69106a
Shivansh Singh <2310shivansh@gmail.com>
2026-09-14 05:16:58 +0530

    Add copyright header to Test.lean

--- LatticeTheory/Test.lean
+++ LatticeTheory/Test.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Shivansh Singh. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shivansh Singh
+-/
+
 import Mathlib.Order.CompleteLattice.Defs
 import Mathlib.Order.WellFounded
 import Mathlib.SetTheory.Ordinal.Basic
